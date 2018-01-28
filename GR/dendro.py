573db42a3a7ebd7b1becefd7057889e5e9a70de5
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2018-01-28 20:55:27 +0530

    Code refactored

--- GR/dendro.py
+++ GR/dendro.py
@@ -13,13 +13,8 @@ from sympy.tensor.array import *
 from sympy.functions.special.tensor_functions import KroneckerDelta
 from sympy.utilities import numbered_symbols
 from sympy.printing import print_ccode, ccode
-from graphviz import Digraph
-import os
-import string
-import random
+#import GR.dependency as dp
 
-#You should install graphviz  and add the bin path to the environment path variable
-#os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 # internal variables
 undef = symbols('undefined')
 
@@ -583,23 +578,7 @@ def generate(ex, vnames, idx):
         ee_syms = numbered_symbols(prefix=ee_name)
         _v = cse(lexp, symbols=ee_syms, optimizations='basic')
 
-        # Making the dependency graph
-
-        for i in _v[0]:
-            expr = Eq(i[0], i[1])
-            generate_dependencies(expr)
-
-        eqn_number = 1
-
-        for i in _v[1]:
-
-            expr = Eq(symbols("Equation" + str(eqn_number)), i)
-            generate_dependencies(expr)
-            eqn_number = eqn_number + 1
-
-        global dot
-        print(dot.source)
-        dot.render('dependency.gv', view=True)
+        #dp.drawgragh(_v)
 
         custom_functions = {'grad': 'grad', 'grad2': 'grad2', 'agrad': 'agrad', 'kograd': 'kograd'}
 
@@ -639,38 +618,6 @@ def generate(ex, vnames, idx):
             print_n_write(st.replace("'",""), output_file)
 
         print_n_write('// Dendro vectorized code: }}} ', output_file)
-
-variable_counter = 0
-dot = Digraph(comment='Dependency Graph')
-nodes = []
-
-
-def generate_dependencies(expression):
-    global dot
-    global nodes
-    global variable_counter
-    args = expression.args
-    func = expression.func
-    variable_counter = variable_counter + 1
-    node_name = "A" + str(variable_counter)
-    dot.node( node_name, str(func))
-    for arg in args:
-        if(len(arg .args)==0):
-            if(str(arg).startswith("DENDRO") and  (str(arg) in nodes)):
-                dot.edge(node_name, str(arg))
-            elif (str(arg).startswith("DENDRO") and (str(arg) not in nodes)):
-                dot.node(str(arg), str(arg))
-                nodes.append(str(arg))
-                dot.edge(node_name, str(arg))
-            else:
-                variable_counter = variable_counter + 1
-                dot.node(str(arg)+str(variable_counter), str(arg))
-                dot.edge(node_name, str(arg))
-        else:
-
-            dot.edge(node_name, generate_dependencies(arg))
-
-    return node_name
 
 def replace_pow(exp_in):
     """
