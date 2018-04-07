5b50389deb56e67f9c58e12dbf1502da785fc409
Eminda <eminda.14@cse.mrt.ac.lk>
2018-04-07 14:53:03 +0530

    bssneqn solve by part by part gpu scheduling

--- bssn/cuda_src/rhs_cuda.cu
+++ bssn/cuda_src/rhs_cuda.cu
@@ -121,7 +121,6 @@ const unsigned int& bflag)
     // test_file_write::writeToFile("output_cuda.txt", host_array_cpu, unzip_dof*24);
     // free(host_array_cpu);
     #endif
-
     // Free up GPU memory
     #include "bssnrhs_cuda_offset_demalloc.h"
     #include "bssnrhs_cuda_mdealloc.h"
