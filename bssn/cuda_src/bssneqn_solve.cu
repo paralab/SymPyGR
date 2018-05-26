00add10d6abed7b3e703a7130c511ca0cb1aa2ed
ErangaD <erangadulshan.14@cse.mrt.ac.lk>
2018-05-26 19:23:05 +0530

    three dimension thread allocation

--- bssn/cuda_src/bssneqn_solve.cu
+++ bssn/cuda_src/bssneqn_solve.cu
@@ -23,19 +23,19 @@ __global__ void cuda_bssn_eqns_points(int * dev_offset, int * dev_sz, double * d
     #include "list_of_para.h"
     )
 {
-    int id = *dev_offset + blockIdx.x*threads_per_block + threadIdx.x;
+    int i = 3 + threadIdx.x + blockIdx.x * blockDim.x;
+    int j = 3 + threadIdx.y + blockIdx.y * blockDim.y;
+    int k = 3 + threadIdx.z + blockIdx.z * blockDim.z;
 
-    int i = id%(dev_sz[0]-6) + 3;
-    int j = ((id/(dev_sz[0]-6))%(dev_sz[1]-6)) + 3;
-    int k = (id/(dev_sz[2]-6)/(dev_sz[1]-6)) + 3;
-
-    if (k>=dev_sz[2]-3) return;
+    int nx = dev_sz[0]; 
+    int ny = dev_sz[1]; 
+    if(i >= nx-3 || j >= ny-3 || k >= dev_sz[2]-3) return;
 
     double z = dev_pmin[2] + *dev_dy_hz*k;
     double y = dev_pmin[1] + *dev_dy_hy*j;
     double x = dev_pmin[0] + *dev_dy_hx*i;
 
-    int pp = i + (dev_sz[0])*(j + (dev_sz[1])*k);
+    int pp = i + nx*(j + ny*k);
     double r_coord = sqrt(x*x + y*y + z*z);
     double eta = ETA_CONST;
     if (r_coord >= ETA_R0) {
@@ -50,42 +50,27 @@ double * dev_var_in, double * dev_var_out,
 , cudaStream_t stream
 )
 {
-    int total_points = (sz[2]-6)*(sz[1]-6)*(sz[0]-6);
-
-    int points_at_once = threads_per_block_cpu*blocks_cpu;
-    int loops = ceil(1.0*total_points/points_at_once);
-
+    const int ie = sz[0] - 3;//x direction
+    const int je = sz[1] - 3;//y direction
+    const int ke = sz[2] - 3;//z direction
+  
+    int temp_max = (ie>je)? ie : je;
+    int maximumIterations = (temp_max>ke) ? temp_max: ke;
+     
+    int requiredBlocks = (5+maximumIterations) / 6;
+    cuda_bssn_eqns_points<<< dim3(requiredBlocks, requiredBlocks, requiredBlocks),
+    dim3((ie + requiredBlocks -1)/requiredBlocks,
+    (je + requiredBlocks -1)/requiredBlocks, 
+    (ke + requiredBlocks -1)/requiredBlocks), 0, stream >>>(0, dev_sz, dev_pmin, dev_dy_hz, dev_dy_hy, dev_dy_hx, dev_var_in, dev_var_out,
+        #include "list_of_args.h"
+    );
+    
+    // Check for any errors launching the kernel
     cudaError_t cudaStatus;
-    // full
-    for(int i=0; i<loops; i++){
-        int offset = i*points_at_once;
-
-        int * dev_offset;
-        cudaStatus = cudaMalloc((void **) &dev_offset, sizeof(int));
-        if (cudaStatus != cudaSuccess) {fprintf(stderr, "dev_offset cudaMalloc failed!\n"); return;}
-
-        cudaStatus = cudaMemcpyAsync(dev_offset, &offset, sizeof(int), cudaMemcpyHostToDevice, stream);
-        if (cudaStatus != cudaSuccess) {fprintf(stderr, "dev_offset cudaMemcpy failed!\n"); return;}
-
-        cuda_bssn_eqns_points<<< blocks_cpu, threads_per_block_cpu, 0, stream >>>(dev_offset, dev_sz, dev_pmin, dev_dy_hz, dev_dy_hy, dev_dy_hx, dev_var_in, dev_var_out,
-            #include "list_of_args.h"
-        );
-        
-        // Check for any errors launching the kernel
-        cudaError_t cudaStatus;
-        cudaStatus = cudaGetLastError();
-        if (cudaStatus != cudaSuccess) {
-            fprintf(stderr, "cuda_bssn_eqns_points Kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-            return;
-        }
-        cudaStatus = cudaFree(dev_offset);
-        if (cudaStatus != cudaSuccess) { fprintf(stderr, "dev_offset cudaFree failed: %s\n", cudaGetErrorString(cudaStatus));};
-    } 
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
+    cudaStatus = cudaGetLastError();
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching cuda_bssn_eqns_points kernals!\n", cudaStatus);
+        fprintf(stderr, "cuda_bssn_eqns_points Kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
         return;
     }
+   
 }
