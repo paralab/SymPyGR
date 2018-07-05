94caf754db2a3f3721d2bf76a0f76309fdf74f17
akiladperera <akilamatrix@gmail.com>
2018-07-05 20:10:56 +0530

    remove numberofstreams directive

--- bssn/include/GPUConfig.h
+++ bssn/include/GPUConfig.h
@@ -1,10 +1,8 @@
 #define threads_per_block 1024
 #define threads_per_block_rhs 250
 
-#define thread_load_deriv 5
-#define thread_load_adv_deriv 5
+#define thread_load_deriv 1
+#define thread_load_adv_deriv 1
 #define thread_load_rhs 1
-#define thread_load_ko_deriv 5
+#define thread_load_ko_deriv 1
 #define thread_load_output 1
-
-#define numberOfStreams 2
