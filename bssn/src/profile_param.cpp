5a17ca8163cf35dc93f9fbe572afd751154b8f0a
akiladperera <akilamatrix@gmail.com>
2018-07-07 22:05:34 +0530

    change eff bandwidth calc

--- bssn/src/profile_param.cpp
+++ bssn/src/profile_param.cpp
@@ -68,12 +68,12 @@ void bssn::timer::profileInfo()
     std::cout << std::left << std::setw(nameWidth) << std::setfill(separator) <<"--kernel_memcopy(s)";
     std::cout << std::left << std::setw(nameWidth) << std::setfill(separator) << t_stat <<std::endl;
 
-    t_stat=t_memcopy.seconds;
-    std::cout << std::left << std::setw(nameWidth) << std::setfill(separator) <<"--memcopy(s)";
-    std::cout << std::left << std::setw(nameWidth) << std::setfill(separator) << t_stat <<std::endl;
+    // t_stat=t_memcopy.seconds;
+    // std::cout << std::left << std::setw(nameWidth) << std::setfill(separator) <<"--memcopy(s)";
+    // std::cout << std::left << std::setw(nameWidth) << std::setfill(separator) << t_stat <<std::endl;
 
-    std::cout << std::left << std::setw(nameWidth) << std::setfill(separator) <<"--kernel(s)";
-    std::cout << std::left << std::setw(nameWidth) << std::setfill(separator) << t_memcopy_kernel.seconds-t_memcopy.seconds <<std::endl;
+    // std::cout << std::left << std::setw(nameWidth) << std::setfill(separator) <<"--kernel(s)";
+    // std::cout << std::left << std::setw(nameWidth) << std::setfill(separator) << t_memcopy_kernel.seconds-t_memcopy.seconds <<std::endl;
 
 
     std::cout<<"Speedup"<<std::endl;
@@ -84,6 +84,6 @@ void bssn::timer::profileInfo()
     std::cout << std::left << std::setw(nameWidth) << std::setfill(separator) <<"  Approx. FlopRate (GFlop/s)";
     std::cout << std::left << std::setw(nameWidth) << std::setfill(separator)<< flop_count.seconds/t_memcopy_kernel.seconds << "Percentage " << (flop_count.seconds/t_memcopy_kernel.seconds)*100/max_dp_flop_rate << "%" << std::endl;
 
-    std::cout << std::left << std::setw(nameWidth) << std::setfill(separator) <<"  Appr. Eff. Bandwidth (GB/s)";
-    std::cout << std::left << std::setw(nameWidth) << std::setfill(separator)<< total_points.seconds*8/1024/1024/1024/t_memcopy.seconds << "Percentage " << (total_points.seconds*8/1024/1024/1024/t_memcopy.seconds*100)/max_bandwidth << "%" << std::endl;
+    std::cout << std::left << std::setw(nameWidth) << std::setfill(separator) <<"  Appr. Bandwidth (GB/s)";
+    std::cout << std::left << std::setw(nameWidth) << std::setfill(separator)<< total_points.seconds*8/1024/1024/1024/t_memcopy_kernel.seconds << "Percentage " << (total_points.seconds*8/1024/1024/1024/t_memcopy_kernel.seconds*100)/max_bandwidth << "%" << std::endl;
 }
