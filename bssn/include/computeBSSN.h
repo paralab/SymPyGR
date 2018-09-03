39d0e637952083a41f2e1851ef7787d041ef5538
akiladperera <akilamatrix@gmail.com>
2018-09-04 00:29:04 +0530

    change the flop count

--- bssn/include/computeBSSN.h
+++ bssn/include/computeBSSN.h
@@ -34,7 +34,7 @@ const unsigned int BSSN_NUM_VARS=24;
 const unsigned int PAD_WIDTH=3;
 
 int steamCountToLevel[5] = {1, 1, 1, 1, 1};
-double MegaflopCount[6] = {3.95, 7.72, 40.71, 282.61, 2095.15};
+double MegaflopCount[6] = {0.4245, 2.475684, 16.684548, 122.042052, 932.6265, 7290.155844};
 int hybrid_divider = 3; // divider must be smaller than the length of steamCountToLevel
 
 // {30.564, 0.044148, 0.071316, 0.125652, 0.234324};
