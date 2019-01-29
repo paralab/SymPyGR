ca94b81c665a1d5a30ec8ab87af40403b3eaac26
milindasf <milinda@cs.utah.edu>
2019-01-29 14:27:54 -0800

    simple example for staged and unstaged versions of bssn equations.

---
+++ bssn/examples/include/rhsTest.h
@@ -0,0 +1,30 @@
+/**
+ * @author: Milinda Fernando
+ * @breif: Contains simple file to compute the staged and unstaged version of the 
+ * generated RHS code. 
+ */
+
+
+#ifndef DENDRO_5_0_RHSTEST_H
+#define DENDRO_5_0_RHSTEST_H
+
+#include <iostream>
+#include <random>
+#include "block.h"
+#include <vector>
+#include "profile_param.h"
+#include <grUtils.h>
+#include "rhs.h"
+#include "block_cu.h"
+#include "parameters.h"
+#include "point.h"
+#include "grDef.h"
+#include <chrono>
+
+
+typedef std::chrono::high_resolution_clock Time;
+typedef std::chrono::milliseconds ms;
+typedef std::chrono::duration<float> fsec;
+
+
+#endif
