a199860200c7a7feb42692946c1db124aa37886c
ljmoyn <ljmoyn@gmail.com>
2020-01-10 15:45:43 -0700

    minor redesign and cleanup of python codegen

--- bssn/CodeGen/nlsmParams.py
+++ bssn/CodeGen/nlsmParams.py
@@ -9,75 +9,80 @@ parameters.add("__comment__(Evolution variable indices )", "U_CHI=0,U_PHI=1")
 parameters.add("DENDRO_VERSION", 5.0)
 
 parameters.setCategory("CONSTANTS")
-parameters.add(PresetParam.ELE_ORDER, 4)
+parameters.add(RequiredParameter.ELE_ORDER, 4)
 
 parameters.setCategory("IO")
-parameters.add(PresetParam.RESTORE_SOLVER, 0)
-parameters.add(PresetParam.IO_OUTPUT_FREQ, 10)
-parameters.add(PresetParam.TIME_STEP_OUTPUT_FREQ,10)
-parameters.add(PresetParam.REMESH_TEST_FREQ, 10)
-parameters.add(PresetParam.CHECKPT_FREQ, 10000)
-parameters.add(PresetParam.IO_OUTPUT_GAP, 1)
-parameters.add(PresetParam.VTU_FILE_PREFIX, "nlsm_gr")
-parameters.add(PresetParam.CHKPT_FILE_PREFIX, "nlsm_cp")
-parameters.add(PresetParam.PROFILE_FILE_PREFIX, "nlsm_prof")
-parameters.add(PresetParam.NUM_EVOL_VARS_VTU_OUTPUT, 2)
-parameters.add(PresetParam.VTU_OUTPUT_EVOL_INDICES, [0, 1])
+parameters.add(RequiredParameter.RESTORE_SOLVER, 0)
+parameters.add(RequiredParameter.IO_OUTPUT_FREQ, 10)
+parameters.add(RequiredParameter.TIME_STEP_OUTPUT_FREQ,10)
+parameters.add(RequiredParameter.REMESH_TEST_FREQ, 10)
+parameters.add(RequiredParameter.CHECKPT_FREQ, 10000)
+parameters.add(RequiredParameter.IO_OUTPUT_GAP, 1)
+
+#for scratch output when running on the cluster
+#parameters.add(RequiredParameter.VTU_FILE_PREFIX, "/scratch/kingspeak/serial/{uid}/nlsm_gr")
+#parameters.add(RequiredParameter.CHKPT_FILE_PREFIX, "/scratch/kingspeak/serial/{uid}/nlsm_cp")
+#parameters.add(RequiredParameter.PROFILE_FILE_PREFIX, "/scratch/kingspeak/serial/{uid}/nlsm_prof")
+parameters.add(RequiredParameter.VTU_FILE_PREFIX, "nlsm_gr")
+parameters.add(RequiredParameter.CHKPT_FILE_PREFIX, "nlsm_cp")
+parameters.add(RequiredParameter.PROFILE_FILE_PREFIX, "nlsm_prof")
+parameters.add(RequiredParameter.NUM_EVOL_VARS_VTU_OUTPUT, 2)
+parameters.add(RequiredParameter.VTU_OUTPUT_EVOL_INDICES, [0, 1])
 
 parameters.setCategory("LOAD BALANCING & MESH")
-parameters.add(PresetParam.DENDRO_GRAIN_SZ, 100)
-parameters.add(PresetParam.ASYNC_COMM_K, 2)
-parameters.add(PresetParam.DENDRO_AMR_FAC, 1.0)
-parameters.add(PresetParam.LOAD_IMB_TOL, .1)
-parameters.add(PresetParam.DIM, 3)
-parameters.add(PresetParam.MAXDEPTH, 6)
-parameters.add(PresetParam.SPLIT_FIX, 2)
+parameters.add(RequiredParameter.DENDRO_GRAIN_SZ, 100)
+parameters.add(RequiredParameter.ASYNC_COMM_K, 2)
+parameters.add(RequiredParameter.DENDRO_AMR_FAC, 1.0)
+parameters.add(RequiredParameter.LOAD_IMB_TOL, .1)
+parameters.add(RequiredParameter.DIM, 3)
+parameters.add(RequiredParameter.MAXDEPTH, 10)
+parameters.add(RequiredParameter.SPLIT_FIX, 2)
 
 parameters.setCategory("WAVELET REFINEMENT")
-parameters.add(PresetParam.NUM_REFINE_VARS, 2)
-parameters.add(PresetParam.REFINE_VARIABLE_INDICES, [0,1])
+parameters.add(RequiredParameter.NUM_REFINE_VARS, 2)
+parameters.add(RequiredParameter.REFINE_VARIABLE_INDICES, [0,1])
 
 parameters.setCategory("GRID")
-parameters.add(PresetParam.GRID_MIN_X, -200.0)
-parameters.add(PresetParam.GRID_MAX_X, 200.0)
-parameters.add(PresetParam.GRID_MIN_Y, -200.0)
-parameters.add(PresetParam.GRID_MAX_Y, 200.0)
-parameters.add(PresetParam.GRID_MIN_Z, -200.0)
-parameters.add(PresetParam.GRID_MAX_Z, 200.0)
+parameters.add(RequiredParameter.GRID_MIN_X, -200.0)
+parameters.add(RequiredParameter.GRID_MAX_X, 200.0)
+parameters.add(RequiredParameter.GRID_MIN_Y, -200.0)
+parameters.add(RequiredParameter.GRID_MAX_Y, 200.0)
+parameters.add(RequiredParameter.GRID_MIN_Z, -200.0)
+parameters.add(RequiredParameter.GRID_MAX_Z, 200.0)
 
 parameters.setCategory("RK SOLVER")
-parameters.add(PresetParam.RK45_TIME_BEGIN, 0)
-parameters.add(PresetParam.RK45_TIME_END, 10)
+parameters.add(RequiredParameter.RK45_TIME_BEGIN, 0)
+parameters.add(RequiredParameter.RK45_TIME_END, 100)
 
-parameters.add(PresetParam.CFL_FACTOR, .1)
+parameters.add(RequiredParameter.CFL_FACTOR, .1)
 
-compd_min = [parameters[PresetParam.GRID_MIN_X].value, parameters[PresetParam.GRID_MIN_Y].value, parameters[PresetParam.GRID_MIN_Z].value]
-parameters.add(PresetParam.COMPD_MIN, compd_min)
+compd_min = [parameters[RequiredParameter.GRID_MIN_X].value, parameters[RequiredParameter.GRID_MIN_Y].value, parameters[RequiredParameter.GRID_MIN_Z].value]
+parameters.add(RequiredParameter.COMPD_MIN, compd_min)
 
-compd_max = [parameters[PresetParam.GRID_MAX_X].value, parameters[PresetParam.GRID_MAX_Y].value, parameters[PresetParam.GRID_MAX_Z].value]
-parameters.add(PresetParam.COMPD_MAX, compd_max)
+compd_max = [parameters[RequiredParameter.GRID_MAX_X].value, parameters[RequiredParameter.GRID_MAX_Y].value, parameters[RequiredParameter.GRID_MAX_Z].value]
+parameters.add(RequiredParameter.COMPD_MAX, compd_max)
 
 octree_min = [0.0,0.0,0.0]
-parameters.add(PresetParam.OCTREE_MIN, octree_min)
+parameters.add(RequiredParameter.OCTREE_MIN, octree_min)
 
-maxdepth = 1<<parameters[PresetParam.MAXDEPTH].value
+maxdepth = 1<<parameters[RequiredParameter.MAXDEPTH].value
 octree_max = [maxdepth,maxdepth,maxdepth]
-parameters.add(PresetParam.OCTREE_MAX, octree_max)
+parameters.add(RequiredParameter.OCTREE_MAX, octree_max)
 
-timestep = parameters[PresetParam.CFL_FACTOR].value * (parameters[PresetParam.COMPD_MAX].value[0] - parameters[PresetParam.COMPD_MIN].value[0]) / (1 << parameters[PresetParam.MAXDEPTH].value)
-parameters.add(PresetParam.RK45_TIME_STEP_SIZE, timestep)
-parameters.add(PresetParam.RK45_DESIRED_TOL, 1e-3)
-parameters.add(PresetParam.KO_DISS_SIGMA, 1e-1)
-parameters.add(PresetParam.RK_MIN_TOL, 1e-4)
+timestep = parameters[RequiredParameter.CFL_FACTOR].value * (parameters[RequiredParameter.COMPD_MAX].value[0] - parameters[RequiredParameter.COMPD_MIN].value[0]) / (1 << parameters[RequiredParameter.MAXDEPTH].value)
+parameters.add(RequiredParameter.RK45_TIME_STEP_SIZE, timestep)
+parameters.add(RequiredParameter.RK45_DESIRED_TOL, 1e-3)
+parameters.add(RequiredParameter.KO_DISS_SIGMA, 1e-1)
+parameters.add(RequiredParameter.RK_MIN_TOL, 1e-5)
 
 parameters.setCategory("BLOCK Adaptivity (Not Recommended use AMR)")
-parameters.add(PresetParam.ENABLE_BLOCK_ADAPTIVITY, 0)
-parameters.add(PresetParam.BLK_MIN_X, -6.0)
-parameters.add(PresetParam.BLK_MIN_Y, -6.0)
-parameters.add(PresetParam.BLK_MIN_Z, -6.0)
-parameters.add(PresetParam.BLK_MAX_X, 6.0)
-parameters.add(PresetParam.BLK_MAX_Y, 6.0)
-parameters.add(PresetParam.BLK_MAX_Z, 6.0)
+parameters.add(RequiredParameter.ENABLE_BLOCK_ADAPTIVITY, 0)
+parameters.add(RequiredParameter.BLK_MIN_X, -6.0)
+parameters.add(RequiredParameter.BLK_MIN_Y, -6.0)
+parameters.add(RequiredParameter.BLK_MIN_Z, -6.0)
+parameters.add(RequiredParameter.BLK_MAX_X, 6.0)
+parameters.add(RequiredParameter.BLK_MAX_Y, 6.0)
+parameters.add(RequiredParameter.BLK_MAX_Z, 6.0)
 
 namespace = "nlsm"
 
--- bssn/CodeGen/parameters.py
+++ bssn/CodeGen/parameters.py
@@ -1,7 +1,7 @@
 import math
-from enum import Enum
 import warnings
 
+from enum import Enum
 import sys    
 import os
 import datetime
@@ -12,11 +12,10 @@ from sympy.codegen.ast import String
 
 from collections import OrderedDict, namedtuple
 
-#holds dict of parameters, contains mostly output functions
 class Parameters:
-	def __init__(self, paramDict=OrderedDict(), initialData=OrderedDict(), varsDict = OrderedDict()):
+	def __init__(self, paramDict=OrderedDict(), initialDataDict=OrderedDict(), varsDict = OrderedDict()):
 		self.paramDict = paramDict
-		self.initialData = initialData
+		self.initialDataDict = initialDataDict
 		self.varsDict = varsDict
 
 		self.indent = "\t"
@@ -39,15 +38,15 @@ class Parameters:
 		if category is None and self.category is not None:
 			category = self.category
 
-		if isinstance(id, PresetParam):
+		if isinstance(id, RequiredParameter):
 			#get pre-made parameter object from enum value
 			param = id.value
 			param.category = category
 			param.setValue(value, cppType, arraySize)
-			#note the dict key will be the PresetParam enum, not a string
+			#note the dict key will be the RequiredParameter enum, not a string
 			self.paramDict[id] = param
 		else:
-			param = Parameter(id, value, description, category, cppType, arraySize)
+			param = Parameter(id, value, type=ParameterType.other, description=description, category=category, cppType=cppType, arraySize=arraySize)
 			self.paramDict[param.id] = param
 
 	def addInitialData(self, id, expression, category = None):
@@ -59,24 +58,25 @@ class Parameters:
 		if isinstance(expression, Expr):
 
 			evalExpression = expression
-			for initialData in self.initialData.values():
-				evalExpression = evalExpression.subs(initialData.symbol,initialData.value)
+			for initialDatum in self.initialDataDict.values():
+				evalExpression = evalExpression.subs(initialDatum.symbol,initialDatum.value)
 			
-			self.initialData[id] = ExpressionParameter(id,evalExpression.evalf(), expression, category)
+			self.initialDataDict[id] = Parameter(id,evalExpression.evalf(), valueExpression=expression, type=ParameterType.initialData, category=category)
 		else:
-			self.initialData[id] = ExpressionParameter(id,expression, category=category)
+			self.initialDataDict[id] = Parameter(id, expression, type=ParameterType.initialData, category=category)
 
-		return self.initialData[id].symbol
+		return self.initialDataDict[id].symbol
 	def addVar(self, id, initialValue, rhsExpression):
 		if isinstance(initialValue, Expr):
 
 			evalExpression = initialValue
-			for var in list(self.varsDict.values()) + list(self.initialData.values()):
+			# var expressions can include initial data 
+			for var in list(self.varsDict.values()) + list(self.initialDataDict.values()):
 				evalExpression = evalExpression.subs(var.symbol,var.value)
 			
-			self.varsDict[id] = Var(id, evalExpression.evalf(), valueExpression=initialValue, rhsExpression=rhsExpression)
+			self.varsDict[id] = Parameter(id, evalExpression.evalf(), valueExpression=initialValue, rhsExpression=rhsExpression, type=ParameterType.var)
 		else:
-			self.varsDict[id] = Var(id, initialValue, rhsExpression = rhsExpression)
+			self.varsDict[id] = Parameter(id, initialValue, rhsExpression = rhsExpression, type=ParameterType.var)
 
 		return self.varsDict[id].symbol
 
@@ -97,7 +97,7 @@ class Parameters:
 	def values(self):
 		return self.paramDict.values()
 	def initialDataValues(self):
-		return self.initialData.values()
+		return self.initialDataDict.values()
 	def varsValues(self):
 		return self.varsDict.values()
 
@@ -127,11 +127,11 @@ class Parameters:
 		outCpp.write("#include \"{0}\"\n".format(hName))
 		outCpp.write("namespace {0}\n{{\n\n".format(namespace))
 
-		for parameter in list(self.paramDict.values()) + list(self.initialData.values()):
+		for parameter in list(self.paramDict.values()) + list(self.initialDataDict.values()):
 			if parameter.isComment():
 				continue
-			outH.write(parameter.toStringH(1))
-			outCpp.write(parameter.toStringCpp(1))
+			outH.write(parameter.toDefinitionStringH(1))
+			outCpp.write(parameter.toDefinitionStringCpp(1))
 
 		outH.write("\tstatic const unsigned int NUM_VARS={0};\n".format(len(self.varsDict)))
 
@@ -169,7 +169,7 @@ class Parameters:
 
 	def writeJson(self, filepath):
 		categories = OrderedDict()
-		for parameter in list(self.paramDict.values()) + list(self.initialData.values()):
+		for parameter in list(self.paramDict.values()) + list(self.initialDataDict.values()):
 			if parameter.category not in categories:
 				categories[parameter.category] = ""
 			categories[parameter.category] += parameter.toStringJson(2)
@@ -202,12 +202,21 @@ class Parameters:
 
 
 class Parameter:
-	def __init__(self, id, value, description=None, category=None, cppType=None, arraySize=None):
+	def __init__(self, id, value, valueExpression=None, rhsExpression=None, type=None, description=None, category=None, cppType=None, arraySize=None):
 		self.id = id
 		self.description = description
 		self.category = category
 		self.indent = "\t"
 		self.setValue(value, cppType, arraySize)
+
+		if type is None:
+			type = ParameterType.other
+
+		self.type = type
+		self.symbol = symbols(id)
+		self.valueExpression = valueExpression
+		self.rhsExpression = rhsExpression
+		self.precomputeDict = OrderedDict()
 
 	#cppType and arraySize may need to be inferred from the value, so handle that all in one function
 	def setValue(self, value, cppType=None, arraySize=None):
@@ -242,6 +251,11 @@ class Parameter:
 		return self.id.startswith("__comment__", 0, 11)
 
 	def toStringJson(self, indentCount=0):
+		#vars will be defined separately so don't need to be included
+		#initial data with a value expression will use it to compute their value, overwriting anything defined in json
+		if self.type is ParameterType.var or self.valueExpression is not None:
+			return ""
+
 		output = ""
 		if (self.description is not None):
 			output += '{0}"           " : "{1}",\n'.format(self.indent * indentCount, self.description)
@@ -256,7 +270,12 @@ class Parameter:
 		return "{0}{1}{0}".format(valueQuote,self.value)
 
 	#get the c++ string definition for this parameter to be used in parameters.h equivalent
-	def toStringH(self, indentCount = 0):
+	def toDefinitionStringH(self, indentCount = 0):
+		#vars will be defined separately so don't need to be included
+		#initial data with a value expression will use it to compute their value, overwriting anything defined here
+		if self.type is ParameterType.var or self.valueExpression is not None:
+			return ""
+
 		output = ""
 		if self.description is not None:
 			output += "{0}//{1}\n".format(self.indent * indentCount, self.description)
@@ -266,7 +285,12 @@ class Parameter:
 		return output
 
 	#get the c++ string definition for this parameter to be used in parameters.cpp equivalent
-	def toStringCpp(self, indentCount = 0):
+	def toDefinitionStringCpp(self, indentCount = 0):
+		#vars will be defined separately so don't need to be included
+		#initial data with a value expression will use it to compute their value, overwriting anything defined here
+		if self.type is ParameterType.var or self.valueExpression is not None:
+			return ""
+
 		output = ""
 		if self.description is not None:
 			output += "{0}//{1}\n".format(self.indent * indentCount, self.description)
@@ -281,45 +305,18 @@ class Parameter:
 			
 		return output
 
-#parameter capable of handling sympy expression as its value
-class ExpressionParameter(Parameter):
-	def __init__(self, id, value, expression=None, category = None):
-		Parameter.__init__(self, id, value, category=category)
-		self.symbol = symbols(id)
-		self.expression = expression
-
 	def getCppExpression(self, expression):
 		return cxxcode(expression, standard="C++11")
-
-	def toStringJson(self, indentCount=0):
-		#params defined with an expression will have their value calculated
-		#in the cpp directly. Do not want to include them in json
-		if self.expression is not None:
-			return ""
-		return Parameter.toStringJson(self, indentCount)
-
-	def toStringCpp(self, indentCount=0):
-		if self.expression is not None:
-			return ""
-		return Parameter.toStringCpp(self, indentCount)
-
-	def toStringH(self, indentCount=0):
-		if self.expression is not None:
-			return ""
-		return Parameter.toStringH(self, indentCount)
-
-class Var(ExpressionParameter):
-	def __init__(self, id, value, valueExpression=None, rhsExpression=None, category = None):
-		Parameter.__init__(self, id, value, category=category)
-		self.symbol = symbols(id)
-		self.expression = valueExpression
-		self.rhs = rhsExpression
-		self.precomputeDict = OrderedDict()
 
 	#func should be a PrecomputeFunc enum
 	def addPrecompute(self, func):
 		#treating this like an ordered set, so just going to use the keys and ignore values
 		self.precomputeDict[func] = None
+
+class ParameterType(Enum):
+	var = 1,
+	initialData = 2,
+	other = 3
 
 class CppType(Enum):
 	unsignedInt = "unsigned int"
@@ -343,42 +340,12 @@ class PrecomputeFunc(Enum):
 		else:
 			return ""
 
-
-class PresetParam(Enum):
-	#note values start out None, will be filled in before added to paramDict
-	DIM = Parameter("DIM", None, description="dimentionality of the octree, (meshing is supported only for 3D)", cppType = CppType.unsignedInt)
-	MAXDEPTH = Parameter("MAXDEPTH", None, description="maximum level of refinement of the mesh", cppType = CppType.unsignedInt)	
-	ASYNC_COMM_K = Parameter("ASYNC_COMM_K", None, "variable group size for the asynchronous unzip operation", cppType = CppType.unsignedInt)
-	CFL_FACTOR = Parameter("CFL_FACTOR", None, description="CFL Factor", cppType = CppType.double)
-	GRID_MIN_X = Parameter("GRID_MIN_X", None, cppType = CppType.double)
-	GRID_MIN_Y = Parameter("GRID_MIN_Y", None, cppType = CppType.double)
-	GRID_MIN_Z = Parameter("GRID_MIN_Z", None, cppType = CppType.double)
-	GRID_MAX_X = Parameter("GRID_MAX_X", None, cppType = CppType.double)
-	GRID_MAX_Y = Parameter("GRID_MAX_Y", None, cppType = CppType.double)
-	GRID_MAX_Z = Parameter("GRID_MAX_Z", None, cppType = CppType.double)
-	COMPD_MIN = Parameter("COMPD_MIN", None, description="Should be an array defined from values of GRID_MIN", cppType = CppType.double)
-	COMPD_MAX = Parameter("COMPD_MAX", None, description="Should be an array defined from values of GRID_MAX", cppType = CppType.double)
-	OCTREE_MIN = Parameter("OCTREE_MIN", None, description="", cppType = CppType.double)
-	OCTREE_MAX = Parameter("OCTREE_MAX", None, description="", cppType = CppType.double)
-	RK45_TIME_STEP_SIZE = Parameter("RK45_TIME_STEP_SIZE", None, description="value should be calculated using the CFL Factor", cppType = CppType.double)
-	RK45_DESIRED_TOL = Parameter("RK45_DESIRED_TOL", None, description="used in adaptive time stepping (not currently used)", cppType = CppType.double)
-	KO_DISS_SIGMA = Parameter("KO_DISS_SIGMA", None, description="Kreiss-Oliger dissipation", cppType = CppType.double)
-	ENABLE_BLOCK_ADAPTIVITY = Parameter("ENABLE_BLOCK_ADAPTIVITY", None, description="Set to 1 disable AMR and use block adaptivity (not recomended).", cppType = CppType.unsignedInt)
-	BLK_MIN_X = Parameter("BLK_MIN_X", None, cppType = CppType.double)
-	BLK_MIN_Y = Parameter("BLK_MIN_Y", None, cppType = CppType.double)
-	BLK_MIN_Z = Parameter("BLK_MIN_Z", None, cppType = CppType.double)
-	BLK_MAX_X = Parameter("BLK_MAX_X", None, cppType = CppType.double)
-	BLK_MAX_Y = Parameter("BLK_MAX_Y", None, cppType = CppType.double)
-	BLK_MAX_Z = Parameter("BLK_MAX_Z", None, cppType = CppType.double)
-	NUM_REFINE_VARS = Parameter("NUM_REFINE_VARS", None, description="number of refinement variables", cppType = CppType.unsignedInt)
-	REFINE_VARIABLE_INDICES = Parameter("REFINE_VARIABLE_INDICES", None, description="refinement variable IDs", cppType = CppType.unsignedInt)
-	RK_MIN_TOL = Parameter("RK_MIN_TOL", None, description="RK solver tolerance", cppType = CppType.double)
+#note values start out None, will be filled in before added to paramDict
+class RequiredParameter(Enum):
+	###### CONSTANTS ######
 	ELE_ORDER = Parameter("ELE_ORDER", None, description="element order", cppType = CppType.unsignedInt)
-	DENDRO_GRAIN_SZ = Parameter("DENDRO_GRAIN_SZ", None, description="grain size N/p , Where N number of total octants, p number of active cores", cppType = CppType.unsignedInt)
-	LOAD_IMB_TOL = Parameter("LOAD_IMB_TOL", None, description="dendro load imbalance tolerance for flexible partitioning", cppType = CppType.double)
-	SPLIT_FIX = Parameter("SPLIT_FIX", None, description="Splitter fix value", cppType = CppType.unsignedInt)
-	RK45_TIME_BEGIN = Parameter("RK45_TIME_BEGIN", None, description="simulation time begin", cppType = CppType.double)
-	RK45_TIME_END = Parameter("RK45_TIME_END", None, description="simulation time end", cppType = CppType.double)
+
+	###### IO ######
 	RESTORE_SOLVER = Parameter("RESTORE_SOLVER", None, description="Set to 1 to restore solver from a checkpoint. 0 otherwise", cppType = CppType.unsignedInt)
 	CHKPT_FILE_PREFIX = Parameter("CHKPT_FILE_PREFIX", None, description="file prefix for the checkpoint files", cppType = CppType.string)
 	VTU_FILE_PREFIX = Parameter("VTU_FILE_PREFIX", None, description="file prefix for the vtu files", cppType = CppType.string)
@@ -388,6 +355,48 @@ class PresetParam(Enum):
 	REMESH_TEST_FREQ = Parameter("REMESH_TEST_FREQ", None, description="frequency for remeshing test based on wavelets", cppType = CppType.unsignedInt)
 	CHECKPT_FREQ = Parameter("CHECKPT_FREQ", None, description="frequency for checkpoint output", cppType = CppType.unsignedInt)
 	IO_OUTPUT_GAP = Parameter("IO_OUTPUT_GAP", None, description="VTU file output gap. (Not currently used. Might be usefull in adaptive timestepping)", cppType = CppType.unsignedInt)
-	DENDRO_AMR_FAC = Parameter("DENDRO_AMR_FAC", None, description="dendro coarsening factor, corsent if computed wavelet tol < DENDRO_AMR_FAC*WAVELET_TOL ", cppType = CppType.double)
 	NUM_EVOL_VARS_VTU_OUTPUT = Parameter("NUM_EVOL_VARS_VTU_OUTPUT", None, description="number of variables (evolution) to output in vtu files", cppType = CppType.unsignedInt)
 	VTU_OUTPUT_EVOL_INDICES = Parameter("VTU_OUTPUT_EVOL_INDICES", None, description="evolution variable ids", cppType = CppType.unsignedInt)
+
+	###### LOAD BALANCING & MESH ######
+	ASYNC_COMM_K = Parameter("ASYNC_COMM_K", None, description="variable group size for the asynchronous unzip operation", cppType = CppType.unsignedInt)
+	DENDRO_GRAIN_SZ = Parameter("DENDRO_GRAIN_SZ", None, description="grain size N/p , Where N number of total octants, p number of active cores", cppType = CppType.unsignedInt)
+	DENDRO_AMR_FAC = Parameter("DENDRO_AMR_FAC", None, description="dendro coarsening factor, corsent if computed wavelet tol < DENDRO_AMR_FAC*WAVELET_TOL ", cppType = CppType.double)
+	LOAD_IMB_TOL = Parameter("LOAD_IMB_TOL", None, description="dendro load imbalance tolerance for flexible partitioning", cppType = CppType.double)
+	DIM = Parameter("DIM", None, description="dimentionality of the octree, (meshing is supported only for 3D)", cppType = CppType.unsignedInt)
+	SPLIT_FIX = Parameter("SPLIT_FIX", None, description="Splitter fix value", cppType = CppType.unsignedInt)
+	MAXDEPTH = Parameter("MAXDEPTH", None, description="maximum level of refinement of the mesh", cppType = CppType.unsignedInt)	
+
+	###### GRID ######
+	GRID_MIN_X = Parameter("GRID_MIN_X", None, cppType = CppType.double)
+	GRID_MIN_Y = Parameter("GRID_MIN_Y", None, cppType = CppType.double)
+	GRID_MIN_Z = Parameter("GRID_MIN_Z", None, cppType = CppType.double)
+	GRID_MAX_X = Parameter("GRID_MAX_X", None, cppType = CppType.double)
+	GRID_MAX_Y = Parameter("GRID_MAX_Y", None, cppType = CppType.double)
+	GRID_MAX_Z = Parameter("GRID_MAX_Z", None, cppType = CppType.double)
+
+	###### RK SOLVER ######
+	RK45_TIME_BEGIN = Parameter("RK45_TIME_BEGIN", None, description="simulation time begin", cppType = CppType.double)
+	RK45_TIME_END = Parameter("RK45_TIME_END", None, description="simulation time end", cppType = CppType.double)
+	RK45_TIME_STEP_SIZE = Parameter("RK45_TIME_STEP_SIZE", None, description="value should be calculated using the CFL Factor", cppType = CppType.double)
+	RK45_DESIRED_TOL = Parameter("RK45_DESIRED_TOL", None, description="used in adaptive time stepping (not currently used)", cppType = CppType.double)
+	CFL_FACTOR = Parameter("CFL_FACTOR", None, description="CFL Factor", cppType = CppType.double)
+	COMPD_MIN = Parameter("COMPD_MIN", None, description="Should be an array defined from values of GRID_MIN", cppType = CppType.double)
+	COMPD_MAX = Parameter("COMPD_MAX", None, description="Should be an array defined from values of GRID_MAX", cppType = CppType.double)	
+	OCTREE_MIN = Parameter("OCTREE_MIN", None, description="", cppType = CppType.double)
+	OCTREE_MAX = Parameter("OCTREE_MAX", None, description="", cppType = CppType.double)
+	KO_DISS_SIGMA = Parameter("KO_DISS_SIGMA", None, description="Kreiss-Oliger dissipation", cppType = CppType.double)
+	RK_MIN_TOL = Parameter("RK_MIN_TOL", None, description="RK solver tolerance", cppType = CppType.double)	
+	
+	###### BLOCK ADAPTIVITY ######
+	ENABLE_BLOCK_ADAPTIVITY = Parameter("ENABLE_BLOCK_ADAPTIVITY", None, description="Set to 1 disable AMR and use block adaptivity (not recomended).", cppType = CppType.unsignedInt)
+	BLK_MIN_X = Parameter("BLK_MIN_X", None, cppType = CppType.double)
+	BLK_MIN_Y = Parameter("BLK_MIN_Y", None, cppType = CppType.double)
+	BLK_MIN_Z = Parameter("BLK_MIN_Z", None, cppType = CppType.double)
+	BLK_MAX_X = Parameter("BLK_MAX_X", None, cppType = CppType.double)
+	BLK_MAX_Y = Parameter("BLK_MAX_Y", None, cppType = CppType.double)
+	BLK_MAX_Z = Parameter("BLK_MAX_Z", None, cppType = CppType.double)
+
+	NUM_REFINE_VARS = Parameter("NUM_REFINE_VARS", None, description="number of refinement variables", cppType = CppType.unsignedInt)
+	REFINE_VARIABLE_INDICES = Parameter("REFINE_VARIABLE_INDICES", None, description="refinement variable IDs", cppType = CppType.unsignedInt)
+	
