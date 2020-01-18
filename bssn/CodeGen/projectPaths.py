a139f6b511dd99393a78c3d21a5d92fcfb0349bb
ljmoyn <ljmoyn@gmail.com>
2020-01-18 16:42:26 -0700

    generalize cog imports to easily switch out parameter files

--- bssn/CodeGen/projectPaths.py
+++ bssn/CodeGen/projectPaths.py
@@ -1,6 +1,3 @@
-#the files in CogInputTestBuild are direct copies from Dendro/NLSigma. No cog involved.
-#The point is to just use them to easily test the cogged CMakeList.txt (since the files themselves are all valid and will build)
-#actual cogged src files are in CogInput folder, but until they're all finished the results won't build.
 inputPath = "./SymPyGR/bssn/CodeGen/CogInput/"
 
 #TODO: get this from argv once other stuff is more polished
