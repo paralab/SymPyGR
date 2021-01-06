8f936285b468a77c5c9b40ecb4ea55ea2ae8a85f
Liam Moynihan <ljmoyn@gmail.com>
2021-01-06 16:09:41 -0700

    add staging to derivTest

--- bssn/examples/src/derivTest2.cpp
+++ bssn/examples/src/derivTest2.cpp
@@ -214,42 +214,22 @@ int main(int argc, char **argv)
     double *B_rhs2   =  unzipOut + (bssn::VAR::U_B2*NN);
 
     auto time_start=Time::now();
-    for (unsigned int r=0; r < iter; r++)
+    if(vectorize && !stages)
     {
-        for(unsigned int k=3; k < nz-3; k++)
-        {
-            for(unsigned int j=3; j < ny-3; j++)
-            {
-                #pragma novector if(!vectorize)
-                #pragma vector if(vectorize)
-                #pragma ivdep if(vectorize)
-                for(unsigned int i=3; i < nx-3; i++)
-                {
-                        const double x  =  i * hx;
-                        const double y  =  j * hy;
-                        const double z  =  k * hz;
-
-                        const double r_coord =sqrt(x*x + y*y + z*z);
-                        const double sigma=0.4;
-                        double eta=bssn::ETA_CONST;
-                        if (r_coord >= bssn::ETA_R0) {
-                            eta *= pow( (bssn::ETA_R0/r_coord), bssn::ETA_DAMPING_EXP);
-                        }
-
-                        const unsigned int pp = k*ny*nx + j* nx + i;
-
-                        #include "../../src/bssneqs_eta_const_standard_gauge.cpp"
-                }
-            }
-        }
-        //prevent compiler from optimizing out the iter loop
-        #pragma novector
-        for (unsigned int pp=0; pp < NN*bssn::BSSN_NUM_VARS; pp+=10)
-        {
-            unzipOut[pp] += 1e-6;
-            unzipIn[pp] +=1e-6;
-        }
-    }  
+        #include "./test_loop_vector.cpp"
+    }
+    else if(!vectorize && !stages)
+    {
+        #include "./test_loop_novector.cpp"
+    }
+    else if(vectorize && stages)
+    {
+        #include "./test_loop_vector_stages.cpp"
+    }
+    else if(!vectorize && stages)
+    {
+        #include "./test_loop_novector_stages.cpp"
+    }
 
     auto time_end=Time::now();
     fsec fs = time_end - time_start;
