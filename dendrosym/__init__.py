f880c2c60cf7404a0f5a4d0bf938f6860c727a8e
David Van Komen <69689325+dfvankomen@users.noreply.github.com>
2021-05-10 15:39:07 -0600

    basic init file for package loading

---
+++ dendrosym/__init__.py
@@ -0,0 +1,26 @@
+"""__init__.py
+
+This is the initialization file for the DendroSym Python package.
+It helps us make sure that everything is ready to roll
+"""
+
+# the datatypes that includes all of the sympy pieces
+import dendrosym.dtypes
+# numerical relativity functions
+import dendrosym.nr
+
+# code generation
+import dendrosym.codegen
+# parameter information
+import dendrosym.params
+
+
+# ==== MISC. FUNCTIONS AND OPERATIONS ====
+# the basic memory manager
+import dendrosym.memoryManager
+# nxgraph generation
+import dendrosym.nxgraph
+# reference element information
+import dendrosym.refEl
+# sympy cache simulations
+import dendrosym.sympy_cachesim
